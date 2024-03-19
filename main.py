9d4715e8f5df648fe9e4fe4660e1b65b225f528a
Мария <andreeva-mm03@yandex.ru>
2024-03-19 12:40:13 +0300

    Сделала кнопки, которые генерируются с помощью генератора

--- main.py
+++ main.py
@@ -1,32 +1,48 @@
-from api import api_token
+from api import api_token  # Токен в отдельном файле
 from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters.command import Command
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 import asyncio
 import logging
 import json
+#from keyboards import *
 
 bot = Bot(token=api_token)  # Классические пункты для работы с aiogram
 dp = Dispatcher()
 logging.basicConfig(level=logging.INFO, filename="py_log.log", filemode="a")
 with open('users.json', 'r') as fh:
-    USERS = json.load(fh) # Здесь будем хранить id пользователей
+    USERS = json.load(fh)  # Здесь будем хранить id пользователей
 
-@dp.message(Command("start")) # Реакция на команду старт
+
+@dp.message(Command("start"))  # Реакция на команду старт
 async def send_welcome(message: types.Message):
     if str(message.from_user.id) not in USERS:  # Если это первый старт
         USERS[str(message.from_user.id)] = []  # Добавляем пользователя
         with open('users.json', 'w') as fp:
             json.dump(USERS, fp)  # Сохраняем в json
-        kb = [[types.KeyboardButton(text="📚 Заканчиваю школу")],
-              [types.KeyboardButton(text="👩‍🎓 Хочу в магистратуру")],
-              [types.KeyboardButton(text="👨‍👩‍👧‍👦 Я родитель")],
-              [types.KeyboardButton(text="🙊 Не хочу отвечать")]]  # Создаем кнопки
-        keyboard = types.ReplyKeyboardMarkup(keyboard=kb)
-        await message.answer('Привет, {}. Я бот КНИТУ-КАИ, и я помогу тебе узнать информацию о поступлении в 2024 году.'.format(message.from_user.first_name))
-        await message.answer("Выбери кто ты на клавиатуре внизу.", reply_markup=keyboard)
+        kb = ReplyKeyboardBuilder()
+        for el in ["📚 Заканчиваю школу", "👩‍🎓 Хочу в магистратуру", "👨‍👩‍👧‍👦 Я родитель", "🙊 Не хочу отвечать"]:
+            kb.add(types.KeyboardButton(text=el))
+        kb.adjust(1)
+        await message.answer(
+            'Привет, {}. Я бот КНИТУ-КАИ, и я помогу тебе узнать информацию о поступлении в 2024 году.'.format(
+                message.from_user.first_name))
+        await message.answer("Выбери кто ты на клавиатуре внизу.", reply_markup=kb.as_markup())
     else:
         # Если это не первый старт
-        await message.answer('Мы уже здоровались! Выбери команду на кнопках ниже.')
+        await message.answer('Мы уже здоровались! Выбери команду на кнопках ниже.', reply_markup=kb.as_markup())
+
+
+for el in ["📚 Заканчиваю школу", "👩‍🎓 Хочу в магистратуру", "👨‍👩‍👧‍👦 Я родитель", "🙊 Не хочу отвечать"]:
+    # Пока что упрощенная реакция на каждый выбор
+    @dp.message(F.text == el)
+    async def schoolkid(message: types.Message):
+        kb = ReplyKeyboardBuilder()
+        for el in ["📚 Заканчиваю школу", "👩‍🎓 Хочу в магистратуру", "👨‍👩‍👧‍👦 Я родитель", "🙊 Не хочу отвечать"]:
+            kb.add(types.KeyboardButton(text=el))
+        kb.adjust(1)
+        await message.answer("Приятно познакомиться! Выбери на кнопках интересующий тебя раздел.", reply_markup=kb.as_markup())
+
 
 async def main():
     await dp.start_polling(bot)
